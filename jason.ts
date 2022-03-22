46565e34d4a3943643c009b8a9dd2b0459e27dd3
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-21 20:20:40 -0500

    :art: Format

--- jason.ts
+++ jason.ts
@@ -6,13 +6,13 @@ const { _: files, tabs } = parse(Deno.args);
 const parsedFiles = files.map((file) => file.toString());
 
 const textOfFiles = await Promise.all(
-  parsedFiles.map((file) => Deno.readTextFile(file))
+  parsedFiles.map((file) => Deno.readTextFile(file)),
 );
 
 const formatted = textOfFiles.map((text) => jason(text, { tabs }));
 
 await Promise.all(
-  parsedFiles.map((file, i) => Deno.writeTextFile(file, formatted[i]))
+  parsedFiles.map((file, i) => Deno.writeTextFile(file, formatted[i])),
 );
 
 console.log("Done!");
