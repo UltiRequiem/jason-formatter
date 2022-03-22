b361b6bb53cc3b60b631a96af78e8f1e81498291
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-21 19:58:00 -0500

    :recycle: Move to TypeScript

--- jason.ts
+++ jason.ts
@@ -3,12 +3,16 @@ import { jason } from "./mod.ts";
 
 const { _: files, tabs } = parse(Deno.args);
 
-const textOfFiles = await Promise.all(files.map(Deno.readTextFile));
+const parsedFiles = files.map((file) => file.toString());
+
+const textOfFiles = await Promise.all(
+  parsedFiles.map((file) => Deno.readTextFile(file))
+);
 
 const formatted = textOfFiles.map((text) => jason(text, { tabs }));
 
 await Promise.all(
-  files.map((file, i) => Deno.writeTextFile(file, formatted[i]))
+  parsedFiles.map((file, i) => Deno.writeTextFile(file, formatted[i]))
 );
 
 console.log("Done!");
