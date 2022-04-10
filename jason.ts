c5b5329c668e0789f85ca7951a044e5794170c6c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-09 19:13:09 -0500

    :memo: Update

--- jason.ts
+++ jason.ts
@@ -1,18 +1,32 @@
-import { parse } from "https://deno.land/std@0.133.0/flags/mod.ts";
+import { Command } from "https://deno.land/x/cliffy@v0.22.2/command/mod.ts";
+import { colors } from "https://deno.land/x/cliffy@v0.22.2/ansi/mod.ts";
 import { jason } from "./mod.ts";
 
-const { _: files, tabs } = parse(Deno.args, { alias: { t: "tabs" } });
+const root = new Command()
+  .name("jason")
+  .version("2.1.0")
+  .description("Easily format JSON files.")
+  .arguments("<files...:string>")
+  .option("-t, --tabs", "Quantity of tabs to use.")
+  .option("-v, --verbose", "Log when a file is formatted.");
 
-const parsedFiles = files.map((file) => file.toString());
+const {
+  args: [files],
+  options: { tabs, verbose },
+} = await root.parse(Deno.args);
 
-const textOfFiles = await Promise.all(
-  parsedFiles.map((file) => Deno.readTextFile(file)),
-);
+async function jasonFile(file: string) {
+  const text = await Deno.readTextFile(file);
+  const formatted = jason(text, { tabs });
+  await Deno.writeTextFile(file, formatted);
 
-const formatted = textOfFiles.map((text) => jason(text, { tabs }));
+  if (verbose) {
+    console.log(colors.green(`The file "${colors.blue(file)}" was formatted.`));
+  }
+}
 
-await Promise.all(
-  parsedFiles.map((file, i) => Deno.writeTextFile(file, formatted[i])),
-);
+await Promise.all(files.map(jasonFile));
 
-console.log("Done!");
+if (verbose) {
+  console.log(colors.green("Done!"));
+}
