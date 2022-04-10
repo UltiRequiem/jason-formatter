2a741d3f8cac38bdc1b3c458346d3795fae19070
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-09 19:59:31 -0500

    :wrench: Node Config

--- scripts/node_support.ts
+++ scripts/node_support.ts
@@ -1,9 +1,19 @@
 import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.12/node_support.ts";
 
-buildPackage({
-  repoName: "jason-formatter",
-  description: "Shell utility and Library for formatting JSON files",
-  homepage: "https://jason.js.org",
-  keywords: ["json", "formatter"],
-  version: "2.2.0",
-});
+buildPackage(
+  {
+    repoName: "jason-formatter",
+    description: "Shell utility and Library for formatting JSON files",
+    homepage: "https://jason.js.org",
+    keywords: ["json", "formatter"],
+    version: "2.2.0",
+  },
+  {
+    supportCJS: false,
+    check: false,
+    entryPoints: [
+      "./mod.ts",
+      { path: "./jason.ts", name: "jason", kind: "bin" },
+    ],
+  }
+);
