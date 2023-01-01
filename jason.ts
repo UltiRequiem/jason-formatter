f6d3898a2998340838ed680bfa4d3d62869cfd97
GitHub <noreply@github.com>
2023-01-01 00:02:35 +0000

    :arrow_up: update deno dependencies

--- jason.ts
+++ jason.ts
@@ -1,5 +1,5 @@
-import { Command } from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
-import { colors } from "https://deno.land/x/cliffy@v0.25.4/ansi/mod.ts";
+import { Command } from "https://deno.land/x/cliffy@v0.25.6/command/mod.ts";
+import { colors } from "https://deno.land/x/cliffy@v0.25.6/ansi/mod.ts";
 import { jason } from "./mod.ts";
 
 const root = new Command()
